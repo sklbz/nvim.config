b4444512971ffb8cb1e511db39d46b6d298e8742
sklbz <sokolbozanic@gmail.com>
2024-05-28 22:03:58 +0200

    .

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,8 +1,8 @@
 return {
 	"nvim-lualine/lualine.nvim",
 	config = function()
-		local line_options = require("plugins.config.lualine-options")
-		local sections_options = require("plugins.config.lualine-sections")
+		local line_options = require("plugins.config.lualine.lualine-options")
+		local sections_options = require("plugins.config.lualine.lualine-sections")
 		require("lualine").setup({
 			options = line_options,
 			sections = sections_options,
