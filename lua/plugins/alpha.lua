fcde1f88ed38474e314bbcb14e579621ac8f0c8b
sklbz <sokol.bozanic@tumo.world>
2024-06-02 12:44:13 +0200

    update

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -7,6 +7,7 @@ return {
 	config = function()
 		local alpha = require("alpha")
 		local dashboard = require("alpha.themes.dashboard")
+		local button = dashboard.button
 
 		dashboard.section.header.val = {
 			[[                                                                       ]],
@@ -24,6 +25,14 @@ return {
 			[[                                                                       ]],
 			[[                                                                       ]],
 			[[                                                                       ]],
+		}
+
+		dashboard.section.buttons.val = {
+			button("e", "  New file", "<cmd>ene<CR>"),
+			button("SPC s f", "󰈞  Find file"),
+			button("SPC s .", "  Recently opened files"),
+			button("u", "  Update plugins", "<cmd>Lazy sync<CR>"),
+			button("q", "󰅚  Quit", "<cmd>qa<CR>"),
 		}
 
 		alpha.setup(dashboard.opts)
