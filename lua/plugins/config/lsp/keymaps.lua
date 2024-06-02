10e43f793f4a74a8aded9b493ab0c6c5f934a950
sklbz <sokol.bozanic@tumo.world>
2024-06-02 12:22:23 +0200

    update

--- lua/plugins/config/lsp/keymaps.lua
+++ lua/plugins/config/lsp/keymaps.lua
@@ -1,7 +1,7 @@
 local buf = vim.lsp.buf
 
-vim.keymap.set("n", "<leader>rn", buf.rename, {})
-vim.keymap.set("n", "K", buf.hover, {})
-vim.keymap.set("n", "<leader>gd", buf.definition, {})
-vim.keymap.set("n", "<leader>gr", buf.references, {})
-vim.keymap.set({ "n", "v" }, "<leader>ca", buf.code_action, {})
+vim.keymap.set("n", "<leader>rn", buf.rename, { desc = "[R]e[n]ame" })
+vim.keymap.set("n", "K", buf.hover, { desc = "[K] Hover definition" })
+vim.keymap.set("n", "<leader>gd", buf.definition, { desc = "[G]oto [D]efinition" })
+vim.keymap.set("n", "<leader>gr", buf.references, { desc = "[G]oto [R]eferences" })
+vim.keymap.set({ "n", "v" }, "<leader>ca", buf.code_action, { desc = "[C]ode [A]ction" })
