18a58193b6e57b0afa2ee1e800f6b0ec970db770
sklbz <sokol.bozanic@tumo.world>
2024-10-28 10:28:32 +0100

    update

--- lua/plugins/indent-lines.lua
+++ lua/plugins/indent-lines.lua
@@ -1,8 +1,9 @@
 return {
 	"lukas-reineke/indent-blankline.nvim",
 	event = "BufReadPost",
-	config = function()
-		local ibl = require("ibl")
-		ibl.setup({})
-	end,
+	main = "ibl",
+	---@module "ibl"
+	---@diagnostic disable-next-line: undefined-doc-name
+	---@type ibl.config
+	opts = {},
 }
