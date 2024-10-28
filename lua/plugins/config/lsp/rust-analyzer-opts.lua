8bb07c1b4184516e3f65c97ea791da85be6c9586
sklbz <sokol.bozanic@tumo.world>
2024-10-28 10:15:32 +0100

    update

--- lua/plugins/config/lsp/rust-analyzer-opts.lua
+++ lua/plugins/config/lsp/rust-analyzer-opts.lua
@@ -1,9 +1,7 @@
-local enable_hint = function(_, bufnr)
+return {
+	on_attach = function(_, bufnr)
 		vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
 	end,
-
-return {
-	on_attach = enable_hint,
 	settings = {
 		["rust_analyzer"] = {
 			cargo = {
