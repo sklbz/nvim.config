16fe66aba4ab5e6fee5495525422a918f45b73df
sklbz <sokol.bozanic@tumo.world>
2025-03-07 22:09:16 +0100

    update

--- lua/plugins/config/lualine/lualine-options.lua
+++ lua/plugins/config/lualine/lualine-options.lua
@@ -1,6 +1,5 @@
 return {
 	component_separators = { left = "", right = "" },
 	section_separators = { left = "", right = [[]] },
-	sections = { lualine_a = { "buffers" } },
 	theme = "catppuccin",
 }
--- lua/plugins/config/lualine/lualine-sections.lua
+++ lua/plugins/config/lualine/lualine-sections.lua
@@ -1,6 +1,11 @@
 return {
 	lualine_a = { "mode" },
-	lualine_b = { "branch", "diff", "diagnostics" },
+	lualine_b = { "branch", "diff",
+		{
+			"diagnostics",
+			symbols = { error = ' ', warn = ' ', info = ' ' },
+		}
+	},
 	lualine_c = {
 		{
 			"filetype",
@@ -12,7 +17,10 @@ return {
 			icons_enabled = false,
 		},
 	},
-	lualine_x = { "fileformat" },
+	lualine_x = {},
+	lualine_y = {},
+	lualine_z = {}
+	-- lualine_x = { "fileformat" },
 	-- lualine_y = { "progress" },
 	-- lualine_z = { "location" },
 }
